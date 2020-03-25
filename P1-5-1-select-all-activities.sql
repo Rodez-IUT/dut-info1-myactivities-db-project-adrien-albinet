c3ab871e471ecd8d15446605c665e0ab39f31f4c
Adrien Albinet <adrien.albinet@iut-rodez.fr>
2020-03-25 13:54:16 +0100

    fix #2 requete P1-5.1 sur activity

---
+++ P1-5-1-select-all-activities.sql
@@ -0,0 +1,4 @@
+SELECT act.title, act.description, usr.username
+FROM activity act LEFT OUTER JOIN "user" usr ON act.owner_id = usr.id
+WHERE creation_date < '01/09/2019'
+ORDER BY title, username ASC

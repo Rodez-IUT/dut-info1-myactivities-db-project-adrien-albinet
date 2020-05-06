7573707443b3a35d0e1b8baf3ce40d5f39c4ae26
Adrien Albinet <adrien.albinet@iut-rodez.fr>
2020-05-06 11:06:13 +0200

    fix #13 fonction pl_calcule_temps_occupation modifiée

---
+++ P5-1-pl_calcule_temps_occupation.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE FUNCTION gds.pl_calcule_temps_occupation (un_nom_de_salle varchar(250)) RETURNS interval AS $$
+DECLARE
+    un_numero_de_salle bigint;
+    reservation_recurrente gds.reservation%ROWTYPE;
+    calcul interval := 0; -- init
+BEGIN
+    SELECT id INTO un_numero_de_salle FROM gds.salle WHERE nom = un_nom_de_salle;
+    IF FOUND
+        THEN
+            FOR reservation_recurrente IN (SELECT * FROM gds.reservation WHERE salle_id = un_numero_de_salle) LOOP
+                calcul := calcul + AGE(reservation_recurrente.date_fin, reservation_recurrente.date_debut);
+            END LOOP;
+        ELSE
+            RAISE NOTICE 'La salle % n est pas valide',un_nom_de_salle
+            USING ERRCODE='invalid_parameter_value';
+            calcul := null;
+        END IF;
+        RETURN calcul;
+END;
+$$ LANGUAGE plpgsql;
